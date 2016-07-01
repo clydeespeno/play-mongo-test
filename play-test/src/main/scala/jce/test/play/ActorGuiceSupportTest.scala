cef5c9c8c62970c60378888ae8c2c29b22b64ee2
clyde <clyde@redmart.com>
2016-07-01 14:03:40 +0800

    bindActors

--- play-test/src/main/scala/jce/test/play/ActorGuiceSupportTest.scala
+++ play-test/src/main/scala/jce/test/play/ActorGuiceSupportTest.scala
@@ -6,6 +6,9 @@ import com.google.inject.name.Names
 import jce.test.akka.OneActorSystemPerTest
 import org.scalatest.Suite
 import play.api.inject.guice.GuiceInjector
+import play.api.libs.concurrent.AkkaGuiceSupport
+
+import scala.reflect.ClassTag
 
 trait ActorGuiceSupportTest extends OneActorSystemPerTest { this: Suite =>
 
@@ -14,6 +17,10 @@ trait ActorGuiceSupportTest extends OneActorSystemPerTest { this: Suite =>
   def modules: Seq[Module] = Seq[Module]()
 
   def getNamedActor(name: String) = injector.getInstance(Key.get(classOf[ActorRef], Names.named(name)))
+
+  def bindActor[T <: Actor : ClassTag](name: String) = new AbstractModule with AkkaGuiceSupport {
+    override def configure(): Unit = bindActor[T](name)
+  }
 
   abstract override protected def beforeAll(): Unit = {
     super.beforeAll()
@@ -32,3 +39,4 @@ trait ActorGuiceSupportTest extends OneActorSystemPerTest { this: Suite =>
     }
   }
 }
+

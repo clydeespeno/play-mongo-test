e6b8d1fdb0976ec8e5b429dafb75591653f2b2a0
clyde <clyde@redmart.com>
2016-07-01 14:57:43 +0800

    Use finite duration

--- akka-test/src/main/scala/jce/test/akka/AkkaTestUtils.scala
+++ akka-test/src/main/scala/jce/test/akka/AkkaTestUtils.scala
@@ -1,6 +1,7 @@
 package jce.test.akka
 
 import akka.actor.ActorRef
+import akka.util.Timeout
 
 import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext, Future}
@@ -11,14 +12,14 @@ trait AkkaTestUtils {
     import akka.pattern.ask
 
     def askAndGet[T](
-        any: Any, duration: Duration = VeryLongTime)(
+        any: Any, duration: FiniteDuration = VeryLongTime)(
         implicit ctx: ExecutionContext, classTag: ClassTag[T]): T =
-      awaitResult((ref ? any).mapTo[T])
+      awaitResult(ref.?(any)(timeout = Timeout(duration)).mapTo[T])
   }
 
-  def awaitResult[T](f: Future[T], duration: Duration = VeryLongTime): T = {
+  def awaitResult[T](f: Future[T], duration: FiniteDuration = VeryLongTime): T = {
     Await.result(f, duration)
   }
 
-  val VeryLongTime = Long.MaxValue.nanos
+  val VeryLongTime = 91250.days
 }

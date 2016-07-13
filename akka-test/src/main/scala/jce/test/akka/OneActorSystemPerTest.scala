a4d352db8a85b5f307ae3913c741903510146b0b
clyde <clyde@redmart.com>
2016-07-13 14:46:26 +0800

    use actor of with name

--- akka-test/src/main/scala/jce/test/akka/OneActorSystemPerTest.scala
+++ akka-test/src/main/scala/jce/test/akka/OneActorSystemPerTest.scala
@@ -4,8 +4,10 @@ import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.StrictLogging
 import org.scalatest.{BeforeAndAfterAll, Suite}
+
 import scala.concurrent.Await
 import scala.concurrent.duration._
+import scala.reflect.ClassTag
 
 trait OneActorSystemPerTest extends BeforeAndAfterAll with StrictLogging { this: Suite =>
 
@@ -23,7 +25,17 @@ trait OneActorSystemPerTest extends BeforeAndAfterAll with StrictLogging { this:
 
   final implicit val system = createActorSystem()
 
-  final def actorOf[T <: Actor](c: Class[T], args: Any*): ActorRef = system.actorOf(Props(c, args: _*))
+  def actorOf[T <: Actor : ClassTag](args: Any*): ActorRef = createActorRef[T](args)()
+
+  def actorOf[T <: Actor : ClassTag](name: String, args: Any*) = createActorRef[T](args)(name)
+
+  private def createActorRef[T <: Actor : ClassTag](args: Any*)(name: String = ""): ActorRef = {
+    val props = Props(implicitly[ClassTag[T]].runtimeClass, args: _*)
+    name match {
+      case "" => system.actorOf(props)
+      case _ => system.actorOf(props, name)
+    }
+  }
 
   def akkaTestConfigFile = "akka-test.conf"
 
